c0b587ffe78617af8ce5728566610cabd91aae2f
Sam Chesney <samc@xmos.com>
2016-08-08 18:33:33 +0100

    Update copyright with first and latest publication dates

--- lib_sdram/api/sdram.h
+++ lib_sdram/api/sdram.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, XMOS Ltd, All rights reserved
+// Copyright (c) 2014-2016, XMOS Ltd, All rights reserved
 #ifndef SDRAM_H_
 #define SDRAM_H_
 #include <platform.h>
--- lib_sdram/src/structs_and_enums.h
+++ lib_sdram/src/structs_and_enums.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, XMOS Ltd, All rights reserved
+// Copyright (c) 2014-2016, XMOS Ltd, All rights reserved
 #ifndef STRUCTS_AND_ENUMS_H_
 #define STRUCTS_AND_ENUMS_H_
 
